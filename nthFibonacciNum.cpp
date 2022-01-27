37f87b0a03d0f7fc6cffe4650be74b2ac327ef90
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-27 13:38:52 +0530

    Create nthFibonacciNum.cpp

---
+++ nthFibonacciNum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int memo[31] = {0};        //optimised solution
+    int fib(int n) 
+    {
+        if(n<=1)
+            return n;
+        if(memo[n])
+            return memo[n];
+        return memo[n] = fib(n-1) + fib(n-2);
+    }
+};
